44227830c5e028af62ccfec5cc48c509c5f3512e
Robiul Islam <robiulislamrobi0874@gmail.com>
2024-08-15 12:10:22 +0600

    get product from server

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,11 +1,30 @@
 
+import { useEffect, useState } from "react";
+import { axiosCommon } from "../hooks/useAxiosCommon";
 
 const ProductPage = () => {
-	return (
-		<div>
-			produc
-		</div>
-	);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axiosCommon.get('/products');
+        // response.json() করার দরকার নেই, axios এর response থেকে সরাসরি data পাবেন
+        const data = response.data;
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProducts(); // ফাংশনটি কল করা হচ্ছে
+  }, []);
+
+  return (
+    <div>
+      {products.length}
+    </div>
+  );
 };
 
 export default ProductPage;
